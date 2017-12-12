98d0cb05477145fbbdb6b7d380f7457d8fbb80df
archco <archcoster@gmail.com>
2017-12-12 18:09:26 +0900

    modify test file for browser compatibility test.

--- test/element-measurer.test.js
+++ test/element-measurer.test.js
@@ -1,80 +1,84 @@
-const expect = window.chai.expect;
-const Module = window.ElementMeasurer;
-const ElementMeasurer = Module.default;
+var expect = window.chai.expect;
+var Module = window.ElementMeasurer;
+var ElementMeasurer = Module.default;
 
-describe('ElementMeasurer', () => {
-  it('ElementMeasurer is function', () => {
+describe('ElementMeasurer', function () {
+  it('ElementMeasurer is function', function () {
     expect(ElementMeasurer).to.be.a('function');
   });
 
-  describe('constructor', () => {
-    it('"target" argument only document, Element or selector string.', () => {
+  describe('constructor', function () {
+    it('"target" argument only document, Element or selector string.', function () {
       // selector string.
-      let elmMeasurer = new ElementMeasurer('#test-target');
+      var elmMeasurer = new ElementMeasurer('#test-target');
       expect(elmMeasurer).to.be.instanceof(ElementMeasurer);
       expect(elmMeasurer.isDocumentTarget()).to.equal(false);
 
       // document.
-      let docMeasurer = new ElementMeasurer(document);
+      var docMeasurer = new ElementMeasurer(document);
       expect(docMeasurer).to.be.instanceof(ElementMeasurer);
       expect(docMeasurer.isDocumentTarget()).to.equal(true);
 
       // wrong argument.
-      let wrongArg = () => new ElementMeasurer({});
+      var wrongArg = function () {
+        new ElementMeasurer({});
+      };
+
       expect(wrongArg).to.throw(TypeError);
     });
 
-    it('If given value is window, then target as document.documentElement.', () => {
-      let measuerer = new ElementMeasurer(window);
+    it('If given value is window, then target as document.documentElement.', function () {
+      var measuerer = new ElementMeasurer(window);
       expect(measuerer.target).to.equal(document.documentElement);
     });
   });
 
-  describe('#clientWidth and clientHeight', () => {
-    it('Document clientWidth is equal to window.innerWidth.', () => {
-      let docMeasurer = new ElementMeasurer(document);
+  describe('#clientWidth and clientHeight', function () {
+    it('Document clientWidth is equal to window.innerWidth.', function () {
+      var docMeasurer = new ElementMeasurer(document);
       expect(docMeasurer.clientWidth).to.equal(window.innerWidth);
     });
-    it('Element clientHeight is equal to Element.clientHeight.', () => {
-      let elmMeasurer = new ElementMeasurer('#test-target');
-      let elm = document.querySelector('#test-target');
+
+    it('Element clientHeight is equal to Element.clientHeight.', function () {
+      var elmMeasurer = new ElementMeasurer('#test-target');
+      var elm = document.querySelector('#test-target');
       expect(elmMeasurer.clientHeight).to.equal(elm.clientHeight);
     });
   });
 
-  describe('#scrollWidth and scrollHeight', () => {
-    it('scrollHeight is Element.scrollHeight.', () => {
-      let docMeasurer = new ElementMeasurer(document);
-      let html = document.documentElement;
+  describe('#scrollWidth and scrollHeight', function () {
+    it('scrollHeight is Element.scrollHeight.', function () {
+      var docMeasurer = new ElementMeasurer(document);
+      var html = document.documentElement;
       expect(docMeasurer.scrollHeight).to.equal(html.scrollHeight);
     });
   });
 
-  describe('#scrollTop and scrollLeft', () => {
-    it('doc scrollTop is equal to window.pageYOffset.', () => {
-      let docMeasurer = new ElementMeasurer(document);
+  describe('#scrollTop and scrollLeft', function () {
+    it('doc scrollTop is equal to window.pageYOffset.', function () {
+      var docMeasurer = new ElementMeasurer(document);
       expect(docMeasurer.scrollTop).to.equal(window.pageYOffset);
     });
 
-    it('elm scrollLeft is equal to Element.scrollLeft.', () => {
-      let elmMeasurer = new ElementMeasurer('#test-target');
-      let elm = document.querySelector('#test-target');
+    it('elm scrollLeft is equal to Element.scrollLeft.', function () {
+      var elmMeasurer = new ElementMeasurer('#test-target');
+      var elm = document.querySelector('#test-target');
       expect(elmMeasurer.scrollLeft).to.equal(elm.scrollLeft);
     });
   });
 
-  describe('#set scrollTop and set scrollLeft', () => {
+  describe('#set scrollTop and set scrollLeft', function () {
     it('If em.scrollTop set to em.scrollHeight + 200, '
-      + 'then em.scrollTop is maxScrollTop.', () => {
-      let em = new ElementMeasurer('#test-target');
+      + 'then em.scrollTop is maxScrollTop.', function () {
+      var em = new ElementMeasurer('#test-target');
       em.scrollTop = em.scrollHeight + 200;
       expect(em.scrollTop).to.equal(em.maxScrollTop);
     });
   });
 
-  describe('#maxScrollTop and maxScrollLeft', () => {
-    it('maxScrollTop = scrollHeight - clientHeight', () => {
-      let e = new ElementMeasurer(document);
+  describe('#maxScrollTop and maxScrollLeft', function () {
+    it('maxScrollTop = scrollHeight - clientHeight', function () {
+      var e = new ElementMeasurer(document);
       expect(e.maxScrollTop).to.equal(e.scrollHeight - e.clientHeight);
     });
   });
